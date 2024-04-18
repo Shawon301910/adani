9b90b5754348a242e48e797d1be938d6542727c5
Shawon301910 <shawonkumarsarkar.bd@gmail.com>
2024-04-19 00:31:54 +0600

    adani setup complete

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// window scroll functionality -------------------->
+
+window.onscroll = function () { stickyNavbar() };
+
+var navbar = document.getElementById("navbar");
+
+function stickyNavbar() {
+    const nav = document.getElementById("navigation");
+    const hidden = document.getElementById("hidden");
+    const logo = document.getElementById("logo-fn");
+    const bg = document.getElementById("bg-white-fn");
+    if (window.scrollY > 50) {
+        nav.classList.add("fixed");
+        hidden.classList.add("hidden");
+        logo.classList.add("logo-font");
+        bg.classList.add("bg-white");
+    } else {
+        nav.classList.remove("fixed");
+        hidden.classList.remove("hidden");
+        logo.classList.remove("logo-font");
+        bg.classList.remove("bg-white");
+    }
+};
+
+
+// click and show or close nav bar
+document.getElementById("show-nav").addEventListener("click", () => {
+    const showContainer = document.getElementById("show-nav-container");
+    showContainer.style.display = "flex";
+});
+document.getElementById("close-nav").addEventListener("click", () => {
+    const showContainer = document.getElementById("show-nav-container");
+    showContainer.style.display = "none";
+});
